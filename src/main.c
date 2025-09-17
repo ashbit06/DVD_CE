cfe7ec144cb98344776aea5f7feb52a8487869e1
ashbit_ <ashbit0192@gmail.com>
2025-09-17 15:43:08 -0400

    it runs

--- src/main.c
+++ src/main.c
@@ -5,29 +5,29 @@
 #define rand_disc DISCS[rand() % 21]
 #define DISC_SIZE 16
 
-const gfx_sprite_t ***DISCS = {
-    music_disc_11,
-    music_disc_13,
-    music_disc_5,
-    music_disc_blocks,
-    music_disc_cat,
-    music_disc_chirp,
-    music_disc_creator,
-    music_disc_creator_music_box,
-    music_disc_far,
-    music_disc_lava_chicken,
-    music_disc_mall,
-    music_disc_mellohi,
-    music_disc_otherside,
-    music_disc_pigstep,
-    music_disc_precipice,
-    music_disc_relic,
-    music_disc_stal,
-    music_disc_strad,
-    music_disc_tears,
-    music_disc_wait,
-    music_disc_ward
-};
+// const gfx_sprite_t ***DISCS = {
+//     music_disc_11,
+//     music_disc_13,
+//     music_disc_5,
+//     music_disc_blocks,
+//     music_disc_cat,
+//     music_disc_chirp,
+//     music_disc_creator,
+//     music_disc_creator_music_box,
+//     music_disc_far,
+//     music_disc_lava_chicken,
+//     music_disc_mall,
+//     music_disc_mellohi,
+//     music_disc_otherside,
+//     music_disc_pigstep,
+//     music_disc_precipice,
+//     music_disc_relic,
+//     music_disc_stal,
+//     music_disc_strad,
+//     music_disc_tears,
+//     music_disc_wait,
+//     music_disc_ward
+// };
 
 // LCD width = 320
 // LCD height = 240
@@ -39,7 +39,7 @@ int main() {
     gfx_FillScreen(0x00);
 
     gfx_sprite_t *disc = malloc(sizeof(gfx_sprite_t) + DISC_SIZE * DISC_SIZE);
-    disc = rand_disc;
+    disc = music_disc_otherside; //rand_disc;
     int x = (rand() % (320 - 16));
     int y = (rand() % (240 - 16));
     int dx = 1;
@@ -56,33 +56,33 @@ int main() {
         if (x < 0) {
             dx = -dx;
             x = 0;
-            disc = rand_disc;
+            // disc = rand_disc;
 
         }
         if (y < 0) {
             dy = -dy;
             y = 0;
-            disc = rand_disc;
+            // disc = rand_disc;
 
         }
         if (x > (320 - DISC_SIZE*2)) {
             dx = -dx;
             x = 320 - DISC_SIZE*2;
-            disc = rand_disc;
+            // disc = rand_disc;
 
         }
         if (y > (240 - DISC_SIZE*2)) {
             dy = -dy;
             y = 240 - DISC_SIZE*2;
-            disc = rand_disc;
+            // disc = rand_disc;
 
         }
     }
 
     free(disc);
-    for (int i = 0; i < 21; i++) {
-        free(DISCS[i]);
-    }
+    // for (int i = 0; i < 21; i++) {
+    //     free(DISCS[i]);
+    // }
     gfx_End();
     return 0;
 }
