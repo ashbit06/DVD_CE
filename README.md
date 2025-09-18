ea41ed3411052c48a0063711ddfbe6192a055f8b
ashbit_ <ashbit0192@gmail.com>
2025-09-17 23:31:52 -0400

    yeay it fully works now

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-build with `make gfx && make` i guess
+# description
 
-get the tice toolchain
+the iconic dvd screensaver now on your calculator
+
+features all 21 minecraft music discs from 1.21.8
+
+# build
+
+get the [tice toolchain](https://ce-programming.github.io/toolchain/index.html) and build with `make gfx && make`
+
--- src/main.c
+++ src/main.c
@@ -3,42 +3,42 @@
 #include <gfx/gfx.h>
 
 #define rand_disc DISCS[rand() % 21]
-#define DISC_SIZE 16
-
-// const gfx_sprite_t ***DISCS = {
-//     music_disc_11,
-//     music_disc_13,
-//     music_disc_5,
-//     music_disc_blocks,
-//     music_disc_cat,
-//     music_disc_chirp,
-//     music_disc_creator,
-//     music_disc_creator_music_box,
-//     music_disc_far,
-//     music_disc_lava_chicken,
-//     music_disc_mall,
-//     music_disc_mellohi,
-//     music_disc_otherside,
-//     music_disc_pigstep,
-//     music_disc_precipice,
-//     music_disc_relic,
-//     music_disc_stal,
-//     music_disc_strad,
-//     music_disc_tears,
-//     music_disc_wait,
-//     music_disc_ward
-// };
+#define DISC_SIZE 48
+gfx_sprite_t *DISCS[] = {
+    music_disc_11,
+    music_disc_13,
+    music_disc_5,
+    music_disc_blocks,
+    music_disc_cat,
+    music_disc_chirp,
+    music_disc_creator,
+    music_disc_creator_music_box,
+    music_disc_far,
+    music_disc_lava_chicken,
+    music_disc_mall,
+    music_disc_mellohi,
+    music_disc_otherside,
+    music_disc_pigstep,
+    music_disc_precipice,
+    music_disc_relic,
+    music_disc_stal,
+    music_disc_strad,
+    music_disc_tears,
+    music_disc_wait,
+    music_disc_ward
+};
 
 // LCD width = 320
 // LCD height = 240
 
 int main() {
     gfx_Begin();
-    gfx_SetPalette(global_palette, sizeof_global_palette, 0);
-    gfx_SetTransparentColor(0x68);
-    
-    gfx_sprite_t *disc = malloc(sizeof(gfx_sprite_t) + DISC_SIZE * DISC_SIZE);
-    disc = music_disc_otherside; //rand_disc;
+     gfx_SetPalette(global_palette, sizeof_global_palette, 0);
+     gfx_SetTransparentColor(0x68);
+
+     /* Use pointers to the existing sprite data; do not malloc or free these
+         static sprite pointers that come from the generated .c files. */
+     gfx_sprite_t *disc = rand_disc;
     int x = (rand() % (320 - 16));
     int y = (rand() % (240 - 16));
     int dx = 1;
@@ -49,7 +49,7 @@ int main() {
     
     while (!os_GetCSC()) {
         gfx_FillScreen(0x00);
-        gfx_ScaledTransparentSprite_NoClip(disc, x, y, 2, 2);
+        gfx_ScaledTransparentSprite_NoClip(disc, x, y, 3, 3);
         gfx_SwapDraw();
         
         x += dx;
@@ -58,33 +58,31 @@ int main() {
         if (x < 0) {
             dx = -dx;
             x = 0;
-            // disc = rand_disc;
+            disc = rand_disc;
 
         }
         if (y < 0) {
             dy = -dy;
             y = 0;
-            // disc = rand_disc;
+            disc = rand_disc;
 
         }
-        if (x > (320 - DISC_SIZE*2)) {
+        if (x > (320 - DISC_SIZE)) {
             dx = -dx;
-            x = 320 - DISC_SIZE*2;
-            // disc = rand_disc;
+            x = 320 - DISC_SIZE;
+            disc = rand_disc;
 
         }
-        if (y > (240 - DISC_SIZE*2)) {
+        if (y > (240 - DISC_SIZE)) {
             dy = -dy;
-            y = 240 - DISC_SIZE*2;
-            // disc = rand_disc;
+            y = 240 - DISC_SIZE;
+            disc = rand_disc;
 
         }
     }
 
-    free(disc);
-    // for (int i = 0; i < 21; i++) {
-    //     free(DISCS[i]);
-    // }
+    /* The DISCS array and the individual sprite pointers are static/compiled-in
+       and must not be freed. Nothing to free here. */
     gfx_End();
     return 0;
 }
